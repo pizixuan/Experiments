c7a8d076f70cb6fa90e04249a468b4eb96966ce6
rebos <zlf19991008@outlook.com>
2021-01-02 12:35:22 +0800

    每日一题

--- Leetcode/cpp/101.对称二叉树.cpp
+++ Leetcode/cpp/101.对称二叉树.cpp
@@ -17,7 +17,20 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        
+        if (root == nullptr)
+            return true;
+        return checkSymmetric(root->left, root->right);
+    }
+
+    bool checkSymmetric(TreeNode* leftNode, TreeNode* rightNode){
+        if (leftNode == nullptr && rightNode == nullptr)
+            return true;
+        else if (leftNode == nullptr || rightNode == nullptr)
+            return false;
+        else if (leftNode->val == rightNode->val)
+            return checkSymmetric(leftNode->left, rightNode->right) && checkSymmetric(leftNode->right, rightNode->left);
+        else
+            return false;
     }
 };
 // @lc code=end

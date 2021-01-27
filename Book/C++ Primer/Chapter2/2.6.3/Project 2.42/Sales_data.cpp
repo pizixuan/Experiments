4dc13c1f8d0049a0c7574ea084a7ff3a64a12563
Rebos <zlf19991008@outlook.com>
2021-01-27 20:40:51 +0800

    一些调整

--- Book/C++ Primer/Chapter2/2.6.3/Project 2.42/Sales_data.cpp
+++ Book/C++ Primer/Chapter2/2.6.3/Project 2.42/Sales_data.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include<string>
 #include "Sales_data.h"
 
@@ -11,4 +12,23 @@ void Sales_data::disp() {
 	std::cout << "BookNo is " << bookNo << std::endl;
 	std::cout << "units_sold is " << units_sold << std::endl;
 	std::cout << "Revenue is " << revenue << std::endl;
+}
+
+void Sales_data::add(Sales_data data) {
+	if (this->bookNo == data.bookNo) {
+		unsigned totalCnt = this->units_sold + data.units_sold;
+		double totalRevenue = this->revenue + data.revenue;
+		if (totalCnt != 0) {
+			std::cout << "This average price is ";
+			std::cout << totalRevenue / totalCnt << std::endl;
+		}
+		else
+			std::cout << "No Sales!" << std::endl;
+		return;
+	}
+	else {
+		std::cerr << "Data must refer to the same IBSN!" << std::endl;
+		return;
+	}
+	return;
 }

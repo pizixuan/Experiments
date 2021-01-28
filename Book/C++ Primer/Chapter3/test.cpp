568b305970c187a2a34a9970bdd3777958f30f90
Rebos <zlf19991008@outlook.com>
2021-01-28 09:43:01 +0800

    一些调整

--- Book/C++ Primer/Chapter3/test.cpp
+++ Book/C++ Primer/Chapter3/test.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
-#include<string>
-#include<cctype>
 using namespace std;
+
+void quickSortV1(int *, int, int);
+
 int main(){
-	string str("some string!!!");
-	decltype(str.size()) punct_cnt = 0;
-	for (auto c : str)
-		if (ispunct(c))
-			++punct_cnt;
-	cout << punct_cnt
-		 << " punctuation characters in " << str << endl;
-	cout << "=================" << endl;
-	for (auto &c : str)
-		c = toupper(c);
-	cout << str << endl;
+	int list[10] = {7,8,1,3,4,5,2,6,9,0};
+	quickSortV1(list, 0, 9);
+	for (auto i : list)
+		cout << i << " ";
+	return 0;
+}
+
+void quickSortV1(int *arr, int l, int r){
+	if (l >= r)
+		return;
+	int x = l, y = r, z = arr[l];
+	while (x < y){
+		while (x < y && arr[y] >= z)
+			--y;
+		if (x < y)
+			arr[x++] = arr[y];
+		while (x < y && arr[x] <= z)
+			++x;
+		if (x < y)
+			arr[y--] = arr[x];
+	}
+	arr[x] = z;
+	quickSortV1(arr, x, y-1);
+	quickSortV1(arr, x+1, y);
 }

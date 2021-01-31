1e46ab9e4ed36b9d51102752b7e55ff680885a48
Rebos <zlf19991008@outlook.com>
2021-01-31 21:24:12 +0800

    easy

--- PTA/PAT Test/Grade B/1004.cpp
+++ PTA/PAT Test/Grade B/1004.cpp
@@ -1,42 +1,27 @@
-#include<iostream>
+#include <iostream>
 #include<string>
 using namespace std;
-
-struct student {
-	string name;
-	string sNo;
-	int score;
-};
-
 int main() {
+	int max = -1, min = 101,score;
 	int n;
 	cin >> n;
-	student stu[n];
-	student res[2];
-	for (int i = 0; i < n; ++i) {
-		cin >> stu[i].name;
-		cin >> stu[i].sNo;
-		cin >> stu[i].score;
-		if (i == 0)
-			res[0] = stu[0];
-		else if (i == 1) {
-			if (res[0].score >= stu[i].score)
-				res[1] = stu[i]	;
-			else {
-				res[1] = res[0];
-				res[0] = stu[i];
-			}
-		} else {
-			if (stu[i].score > res[0].score) {
-				res[1] = res[0];
-				res[0] = stu[i];
-			} else if (stu[i].score < res[1].score) {
-				res[1] = stu[i];
-			}
+	string name, num;
+	string maxname, maxnum, minname, minnum;
+	for (int i = 0; i < n; i++) {
+		cin >> name >> num >> score;
+		if (score > max) {
+			max = score;
+			maxname = name;
+			maxnum = num;
+		}
+		if (score < min) {
+			min = score;
+			minname = name;
+			minnum = num;
 		}
 	}
-
-	for (int i = 0; i < 2; ++i)
-		cout << res[i].name << " " << res[i].sNo << endl;
+	cout << maxname << " " << maxnum << endl;
+	cout << minname << " " << minnum << endl;
 	return 0;
 }
+

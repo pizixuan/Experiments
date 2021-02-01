f91a0fa56fb5bc6cba4013454cdc9f00f7af6080
Rebos <zlf19991008@outlook.com>
2021-02-01 12:56:54 +0800

    记录学习

--- Book/C++ Primer/Chapter4/4.8/4.25.cpp
+++ Book/C++ Primer/Chapter4/4.8/4.25.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
 	char q = 01110001;
-	cout << (q << 6) << endl;	//64
-	cout << ~(q << 6) << endl;	//-65
+	cout << (q << 6) << endl;	//64 ²¹Âë = ×ÔÉí 
+	cout << ~(q << 6) << endl;	//-65 ²¹Âë = ·ûºÅ£¨·´Âë + 1 £© 
 	return 0;
 }

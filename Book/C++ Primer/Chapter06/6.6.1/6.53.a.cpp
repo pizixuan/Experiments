210057aa2efa03561e91a559c0bb595f29d17e4b
Rebos <zlf19991008@outlook.com>
2021-02-03 15:26:49 +0800

    int & 和 const int & 的重载函数形参

---
+++ Book/C++ Primer/Chapter06/6.6.1/6.53.a.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int calc(int&, int&);
+int calc(const int&, const int&);
+void func() {
+	int num1 = 10;
+	int num2 = 10;
+	cout << calc(num1, num2) << endl;
+	const int num3 = 10;
+	const int num4 = 10;
+	cout << calc(num3, num4) << endl;
+}
+
+int main() {
+	func();
+	return 0;
+}
+
+int calc(int &i, int &j) {
+	return i + j;
+}
+
+int calc(const int &i, const int &j) {
+	return i * j;
+}
